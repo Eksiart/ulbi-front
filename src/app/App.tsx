6edbf74fce2fd87eafc5dc45d0de3f0695830f6b
Eksiart <eksiart-2@mail.ru>
2023-05-12 22:29:23 +0800

    lesson 14 add sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
-import React, {Suspense, useContext, useState} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 const App = () => {
     const {theme} = useTheme();
@@ -14,7 +12,10 @@ const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
-            <AppRouter/>
+            <div className="content-page">
+                <Sidebar/>
+                <AppRouter/>
+            </div>
         </div>
     );
 };
